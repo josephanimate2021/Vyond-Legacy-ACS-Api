9fd8414457f8ea6b80ce5d7d9655ec0adb544a12
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-03-27 16:59:23 -0400

    Update and rename pageRedirect.js to page-genorateNewId.js

---
+++ static/page-genorateNewId.js
@@ -0,0 +1,58 @@
+const fUtil = require("../misc/file");
+const stuff = require("./info");
+const http = require("http");
+
+function toAttrString(table) {
+	return typeof table == "object"
+		? Object.keys(table)
+				.filter((key) => table[key] !== null)
+				.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(table[key])}`)
+				.join("&")
+		: table.replace(/"/g, '\\"');
+}
+function toParamString(table) {
+	return Object.keys(table)
+		.map((key) => `<param name="${key}" value="${toAttrString(table[key])}">`)
+		.join(" ");
+}
+function toObjectString(attrs, params) {
+	return `<object ${Object.keys(attrs)
+		.map((key) => `${key}="${attrs[key].replace(/"/g, '\\"')}"`)
+		.join(" ")}>${toParamString(params)}</object>`;
+}
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
+module.exports = function (req, res, url) {
+	if (req.method != "GET") return;
+	const query = url.query;
+
+	var params;
+	switch (url.pathname) {
+		case "/genorateId": {
+			let presave =
+				query.movieId && query.movieId.startsWith("m")
+					? query.movieId
+					: `m-${fUtil[query.noAutosave ? "getNextFileId" : "fillNextFileId"]("movie-", ".xml")}`;
+			params = {
+				flashvars: {
+					presaveId: presave,
+				},
+				allowScriptAccess: "always",
+				allowFullScreen: "true",
+			};
+			break;
+		}
+
+		default:
+			return;
+	}
+	res.setHeader("Content-Type", "text/html; charset=UTF-8");
+	Object.assign(params.flashvars, query);
+	res.end(`<html><head><script src="https://josephanimate2021.github.io/lvm-static/scripts/genorateId.js"></script><body onload="genorateId()"></body></html>`)
+	return true;
+};
