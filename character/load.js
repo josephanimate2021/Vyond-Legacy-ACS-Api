04cb035e370f77c4b19e5ed46237e2ff7dd1a423
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-10 10:50:56 -0400

    whatever the issue was, it has been taken care of.

--- character/load.js
+++ character/load.js
@@ -22,13 +22,17 @@ module.exports = function (req, res) {
 						console.log("Loading character: "+data.assetId||data.original_asset_id)
 						res.setHeader('Content-Type', 'text/html; charset=UTF-8');
 						process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-						character.load(data.assetId || data.original_asset_id).then(v => { res.statusCode = 200, res.end(0 + v) })
+						character.load(data.assetId || data.original_asset_id)
+							.then(v => { res.statusCode = 200, res.end(0 + v) })
 						//.catch(e => { res.statusCode = 404, res.end(1 + e) })
 						//why send a 404 when you can watch benson on youtube
-							.catch(() => character.load('a-327068826').then(v => {
-							console.log("Couldn't find that character, but it's okay, we loaded Benson instead."),
-								res.statusCode = 200, res.end(0 + v)
-						})).catch(e => {
+							.catch(
+							() => character.load('a-327068826')
+							.then(v => {
+								console.log("Couldn't find that character, but it's okay, we loaded Benson instead."),
+									res.statusCode = 200, res.end(0 + v)
+							})
+						).catch(e => {
 							console.log("But nobody came."),
 								res.statusCode = 404, res.end(1 + e)
 						});
