491bac8422175294be36a90feccbfd8a13f53d1e
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-04-10 11:29:47 -0400

    Update delete.js

--- character/delete.js
+++ character/delete.js
@@ -16,25 +16,12 @@ module.exports = function (req, res) {
 		}
 
 		case 'POST': {
-			if (req.url != '/goapi/getCcCharCompositionXml/') return;
+			if (req.url != '/goapi/deleteAsset/') return;
 			loadPost(req, res).then(async data => {
-				console.log("Loading character: "+data.assetId||data.original_asset_id)
+				console.log("Deleting character: "+data.assetId||data.original_asset_id)
 				res.setHeader('Content-Type', 'text/html; charset=UTF-8');
 				process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
-				character.load(data.assetId || data.original_asset_id)
-					.then(v => { res.statusCode = 200, res.end(0 + v) })
-					//.catch(e => { res.statusCode = 404, res.end(1 + e) })
-					//why send a 404 when you can watch benson on youtube
-					.catch(
-						() => character.load('a-327068826')
-						.then(v => {
-							console.log("Couldn't find that character, but it's okay, we loaded Benson instead."),
-							res.statusCode = 200, res.end(0 + v)
-						})
-					).catch(e => {
-						console.log("But nobody came."),
-						res.statusCode = 404, res.end(1 + e)
-					});
+				character.delete(data.assetId || data.original_asset_id)
 			});
 			return true;
 		}
